27936bdac37a3b7e126c92dd30eef3b3b3725e15
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-07-19 12:25:24 +0200

    Sending job failures into failure channel

--- main.go
+++ main.go
@@ -21,11 +21,11 @@ type Address struct {
 	Description string     `json:"DESCRIPTION"`
 }
 
-func createJobFailure(jobId string) ScannerScaffolding.JobFailure {
+func createJobFailure(jobId, message, details string) ScannerScaffolding.JobFailure {
 	return ScannerScaffolding.JobFailure{
 		JobId:        jobId,
-		ErrorMessage: "GoScanner Failed.",
-		ErrorDetails: "",
+		ErrorMessage: message,
+		ErrorDetails: details,
 	}
 }
 
@@ -61,6 +61,8 @@ func workOnJobs(jobs <-chan ScannerScaffolding.ScanJob, results chan<- ScannerSc
 					u, err := uuid.NewV4()
 					if err != nil {
 						log.Errorf("Could not create UUID for subdomain finding '%s'.", result.Domain)
+						failures <- createJobFailure(job.JobId, "Could not create UUID for finding", "")
+						return
 					}
 
 					addresses := make([]Address, 0)
@@ -94,6 +96,7 @@ func workOnJobs(jobs <-chan ScannerScaffolding.ScanJob, results chan<- ScannerSc
 					findings = append(findings, finding)
 				case <-time.After(2 * time.Hour):
 					log.Warningf("Scan for Job '%s' timed out!", job.JobId)
+					failures <- createJobFailure(job.JobId, "Subdomain Scan Timed out", "Subdomainscans are limited to a two hour timeframe")
 					return
 				}
 			}
