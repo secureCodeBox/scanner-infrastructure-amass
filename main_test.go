7c6ce9623d4a9964b49f793705a61bb07dc699dd
J12934 <13718901+J12934@users.noreply.github.com>
2020-02-19 10:55:15 +0100

    Use testify assertions in tests

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/OWASP/Amass/v3/requests"
-	"github.com/secureCodeBox/scanner-infrastructure-amass/ScannerScaffolding"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAbs(t *testing.T) {
@@ -17,37 +17,12 @@ func TestAbs(t *testing.T) {
 		Source:    "",
 	}
 
-	expectedFinding := ScannerScaffolding.Finding{
-		Name:        "foobar.securecodebox.io",
-		Description: "Found subdomain foobar.securecodebox.io",
-		Location:    "foobar.securecodebox.io",
-		Severity:    "INFORMATIONAL",
-		Category:    "Subdomain",
-		OsiLayer:    "NETWORK",
-	}
-
 	actualFinding := CreateFinding(&res)
 
-	compareFindings(t, expectedFinding, actualFinding)
-}
-
-func compareFindings(t *testing.T, expectedFinding, actualFinding ScannerScaffolding.Finding) {
-	if expectedFinding.Name != actualFinding.Name {
-		t.Errorf("Expected Name to be '%s', was: '%s'", expectedFinding.Name, actualFinding.Name)
-	}
-	if expectedFinding.Description != actualFinding.Description {
-		t.Errorf("Expected Description to be '%s', was: '%s'", expectedFinding.Description, actualFinding.Description)
-	}
-	if expectedFinding.Location != actualFinding.Location {
-		t.Errorf("Expected Location to be '%s', was: '%s'", expectedFinding.Location, actualFinding.Location)
-	}
-	if expectedFinding.Severity != actualFinding.Severity {
-		t.Errorf("Expected Severity to be '%s', was: '%s'", expectedFinding.Severity, actualFinding.Severity)
-	}
-	if expectedFinding.Category != actualFinding.Category {
-		t.Errorf("Expected Category to be '%s', was: '%s'", expectedFinding.Category, actualFinding.Category)
-	}
-	if expectedFinding.OsiLayer != actualFinding.OsiLayer {
-		t.Errorf("Expected OsiLayer to be '%s', was: '%s'", expectedFinding.OsiLayer, actualFinding.OsiLayer)
-	}
+	assert.Equal(t, "foobar.securecodebox.io", actualFinding.Name, "they should be equal")
+	assert.Equal(t, "Found subdomain foobar.securecodebox.io", actualFinding.Description, "they should be equal")
+	assert.Equal(t, "foobar.securecodebox.io", actualFinding.Location, "they should be equal")
+	assert.Equal(t, "INFORMATIONAL", actualFinding.Severity, "they should be equal")
+	assert.Equal(t, "Subdomain", actualFinding.Category, "they should be equal")
+	assert.Equal(t, "NETWORK", actualFinding.OsiLayer, "they should be equal")
 }
